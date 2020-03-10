4d1d1d46280ea09cbcc0dad55e7616e10e41aa2d
Soulwa <awkwardzombienerd@gmail.com>
2020-03-10 19:28:07 -0400

    updated branches, loops for rust 2018

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,10 +1,40 @@
 fn main() {
-    let cond = true;
-    let x = if cond {
+    let number = 6;
+
+    if number < 5 {
+    	println!("true- less than 5");
+    } 
+    else {
+    	println!("false- not less than 5");
+    }
+
+    //cannot do if number { ... }, expression must be bool
+    if number != 0 {
+    	println!("number not zero");
+    }
+
+    //match preferred to else if branches
+    if number % 4 == 0 {
+    	println!("number divisible by 4");
+    }
+    else if number % 3 == 0 {
+    	println!("number divisible by 3");
+    }
+    else if number % 2 == 0 {
+    	println!("number divisible by 2");
+    }
+    else {
+    	println!("number not divisible by 4, 3, or 2");
+    }
+
+    println!("\n");
+
+    let condition = true;
+    let new_number = if condition {
     	5
     } else {
-        6
+    	6
     };
 
-    println!("{}", x);
+    println!("the value of new_number is {}", new_number);
 }
--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,6 +1,53 @@
 fn main() {
-    for i in (1..4).rev() {
-    	println!("{}...", i);
+    let mut counter = 0;
+
+    let result = loop {
+    	counter += 1;
+
+    	if counter == 10 {
+    		break counter * 2;
+    	}
+    };
+
+    println!("result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+    	println!("{}", number);
+    	number -= 1;
     }
-    println!("BLASTOFF!!");
+
+    println!("liftoff!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    //less preferred way to iterate over collection, might go out of bounds, could panic depending on collection, overhead
+    let mut index = 0;
+
+    while index < 5 {
+    	println!("value of element is {}", a[index]);
+
+    	index += 1;
+    }
+
+    //cleaner, safer
+    for elem in a.iter() {
+    	println!("value is {}", elem);
+    }
+
+    //can even do liftoff cleaner, with range
+    for i in (1..4).rev() {
+    	println!("{}", i);
+    }
+    println!("liftoff!");
+
+}
+
+fn loop_examples() {
+
+	//prints "again!" forever in terminal, can't put in main or won't show other examples
+	loop {
+    	println!("again!");
+    }
 }
