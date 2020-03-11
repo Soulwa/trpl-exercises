39b3af612951e550f7e63dc49865de78b63d37c2
Soulwa <awkwardzombienerd@gmail.com>
2020-03-11 14:11:09 -0400

    updated rectangles for rust 2018

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -10,7 +10,7 @@ impl Rectangle {
 	}
 
 	fn can_hold(&self, other: &Rectangle) -> bool {
-		self.width > other.width && self.height > other.height
+		self.width >= other.width && self.height >= other.height
 	}
 
 	fn square(size: u32) -> Rectangle {
@@ -19,13 +19,29 @@ impl Rectangle {
 }
 
 fn main() {
-    let rect = Rectangle { width: 30, height: 50 };
+
+	// declaring width, height as separate variables doesn't show the relation
+	// having rect1 be a tuple doesn't show meaning of values
+	// rect1 as a struct shows meaning of type as well as fields- most clear
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("The area of the rectangle is {} square pixels",
+    		 rect1.area());
+
+    println!("rect1 is {:?}", rect1);
+
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
     let sq = Rectangle::square(3);
 
-    println!("{:?}", sq	);
-    println!("The area of the rectangle is {} square pixels.", rect.area());
-    println!("Rect can hold rect2: {}", rect.can_hold(&rect2));
-    println!("Rect can hold rect3: {}", rect.can_hold(&rect3));
+    println!("sq is {:?}", sq);
+}
+
+// functions like this are better implemented as methods, since it only works for a specific struct
+fn area(rect: &Rectangle) -> u32 {
+	rect.width * rect.height
 }
